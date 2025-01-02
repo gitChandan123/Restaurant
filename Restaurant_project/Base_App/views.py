4192d460ef22efa0af4d95c7558eecb9f2e00ac5
Chandan <chandanvatsa321@gmail.com>
2025-01-02 19:40:47 +0530

    config

--- Restaurant_project/Base_App/views.py
+++ Restaurant_project/Base_App/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
+from Base_App.models import BookTable,AboutUs,feedback,ItemList,Items
 
 # Create your views here.
 def homeView(request):
-    return render(request, 'home.html')
+    items = Items.objects.all()
+    list = ItemList.objects.all()
+    return render(request, 'home.html',{'items':items,'list':list})
 
 def aboutView(request):
-    return render(request, 'about.html')
+    data = AboutUs.objects.all()
+    return render(request, 'about.html',{'data':data})
 
 def menuView(request):
-    return render(request, 'menu.html')
+    items = Items.objects.all()
+    list = ItemList.objects.all()
+    return render(request, 'menu.html',{'items':items,'list':list})
 
 def bookTableView(request):
     return render(request, 'book_table.html')
